d1ce0f07a213a9b3435ac7958d270fc0bfcc38b3
Gustavo <luisinhufut@hotmail.com>
2012-11-02 23:43:42 -0200

    ve ai fael

---
+++ SistemaLoginSQL/frmLogin.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SistemaLoginSQL
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Login = new System.Windows.Forms.TextBox();
+            this.textBox_Senha = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_Entrar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login";
+            // 
+            // textBox_Login
+            // 
+            this.textBox_Login.Location = new System.Drawing.Point(15, 151);
+            this.textBox_Login.Name = "textBox_Login";
+            this.textBox_Login.Size = new System.Drawing.Size(156, 20);
+            this.textBox_Login.TabIndex = 2;
+            // 
+            // textBox_Senha
+            // 
+            this.textBox_Senha.Location = new System.Drawing.Point(15, 201);
+            this.textBox_Senha.Name = "textBox_Senha";
+            this.textBox_Senha.Size = new System.Drawing.Size(156, 20);
+            this.textBox_Senha.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha";
+            // 
+            // button_Entrar
+            // 
+            this.button_Entrar.Location = new System.Drawing.Point(15, 240);
+            this.button_Entrar.Name = "button_Entrar";
+            this.button_Entrar.Size = new System.Drawing.Size(75, 23);
+            this.button_Entrar.TabIndex = 5;
+            this.button_Entrar.Text = "Entrar";
+            this.button_Entrar.UseVisualStyleBackColor = true;
+            this.button_Entrar.Click += new System.EventHandler(this.button_Entrar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 275);
+            this.Controls.Add(this.button_Entrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Senha);
+            this.Controls.Add(this.textBox_Login);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Login;
+        private System.Windows.Forms.TextBox textBox_Senha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_Entrar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ SistemaLoginSQL/frmLogin.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SistemaLoginSQL
+{
+    public partial class frmLogin : Form
+    {
+        //minha conexao com o banco de dados
+        SqlConnection conexao = new SqlConnection();
+        private string strSQL = string.Empty;
+        //string de conexao
+        private string strConn = "Data Source=(local); Initial Catalog=Controle; Integrated Security=SSPI";
+
+        //variavel para controlar se esta logado ou nao, a principio ela começara sem logar.
+        public bool logado = false;
+        
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void logar()
+        {
+            conexao = new SqlConnection(strConn);
+            string login, senha;
+
+            try
+            {
+                login = textBox_Login.Text;
+                senha = textBox_Senha.Text;
+                strSQL = "SELECT COUNT(idUsuario) FROM Usuarios WHERE Login = @L AND Senha = @S";
+                SqlCommand comando = new SqlCommand(strSQL, conexao);
+                comando.Parameters.Add("@L", SqlDbType.VarChar).Value = login;
+                comando.Parameters.Add("@S", SqlDbType.VarChar).Value = senha;
+                //abrindo conexão
+                conexao.Open();
+
+                int v = (int)comando.ExecuteScalar();
+                if (v > 0)
+                {
+                    logado = true;
+                    this.Dispose();
+                }
+                else
+                {
+                    logado = false;
+                    MessageBox.Show("Erro");
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
+
+        private void button_Entrar_Click(object sender, EventArgs e)
+        {
+            if (textBox_Login.Text == "" || textBox_Senha.Text == "")
+            {
+                if (textBox_Login.Text == "" && textBox_Senha.Text == "")
+                {
+                    errorProvider1.SetError(textBox_Login, "*Campo obrigatório, por favor preencha");
+                    errorProvider1.SetError(textBox_Senha, "*Campo obrigatório, por favor preencha");
+
+                }
+                else
+                {
+                    errorProvider1.SetError(textBox_Login, "");
+                    errorProvider1.SetError(textBox_Senha, "");
+                }
+                if (textBox_Senha.Text == "")
+                     errorProvider1.SetError(textBox_Senha, "*Campo obrigatório, por favor preencha");
+                 else
+                     errorProvider1.SetError(textBox_Senha, "");
+                if (textBox_Login.Text == "")
+                    errorProvider1.SetError(textBox_Login, "*Campo obrigatório, por favor preencha");
+                else
+                    errorProvider1.SetError(textBox_Login, "");
+            }
+            else
+            {
+                errorProvider1.SetError(textBox_Senha, "");
+                logar();
+            }
+        }
+    }
+}
